131961ca7934d6c527cc288b1a1ae27364701cb2
tryqylz <tryqylz@outlook.com>
2020-08-15 08:41:58 +0800

    创建了基础Buff.cs
    创建了基础Buff类

---
+++ unity_Project/GJ2020/Assets/Scripts/Buff.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff
+{ 
+    /// <summary>
+    /// 状态ID
+    /// </summary>
+    public int id = -1;
+    /// <summary>
+    /// 状态名
+    /// </summary>
+    public string name = string.Empty;
+    /// <summary>
+    /// 回合数
+    /// </summary>
+    public int roundNum = 1;
+
+    public enum BuffType
+    {
+        Buff, DeBuff
+    }
+    /// <summary>
+    /// buff类型
+    /// </summary>
+    public BuffType type = BuffType.Buff;
+
+    /// <summary>
+    /// Spine Asset 文件名
+    /// </summary>
+    public string spineFileName = string.Empty;
+
+
+
+    /// <summary>
+    /// 被创建时执行
+    /// </summary>
+    public void onCreated()
+    {
+
+    }
+
+    /// <summary>
+    /// 回合开始时被执行
+    /// </summary>
+    public void onRoundStart()
+    {
+
+    }
+
+    /// <summary>
+    /// 回合结束时被执行
+    /// </summary>
+    public void onRoundEnd()
+    {
+
+    }
+
+    /// <summary>
+    /// 被移除时执行
+    /// </summary>
+    public void onRemove()
+    {
+
+    }
+
+    /// <summary>
+    /// 在过期时执行
+    /// </summary>
+    public void onExpired()
+    {
+
+    }
+}
