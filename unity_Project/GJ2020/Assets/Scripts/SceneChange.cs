b68299a2655facd6824052c0ad708cec64426f09
Katyusha444 <52458638+Katyusha444@users.noreply.github.com>
2020-08-16 06:27:01 +0800

    场景试验版本

--- unity_Project/GJ2020/Assets/Scripts/SceneChange.cs
+++ unity_Project/GJ2020/Assets/Scripts/SceneChange.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine;
+using Spine.Unity;
 
-public class SceneChange 
+public class SceneChange :MonoBehaviour 
 {
     public int ID;
     /// <summary>丧阈值 </summary>
@@ -11,4 +13,80 @@ public class SceneChange
     public string imagePath;
 
     public string spineActionName;
+
+
+
+    public SkeletonAnimation ske;
+
+
+    float timer;
+    float timed;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            RoundManager.self.playerActor.sanValue -= 20;
+        }
+    }
+
+    bool change=false;
+    public void Start()
+    {
+        RoundManager.self.playerActor.sanChangeEvent += ChangeSceneSpine;
+        if (change)
+        {
+            Change();
+        }
+    }
+
+    int anim=1;
+    public void ChangeSceneSpine(int san,int nul)
+    {
+        int temp=0;
+        if (RoundManager.self.playerActor.sanValue>=80)
+        {
+            temp = 1;
+        }
+        if (RoundManager.self.playerActor.sanValue >= 60&& RoundManager.self.playerActor.sanValue < 80)
+        {
+            temp = 2;
+       
+        }
+        if (RoundManager.self.playerActor.sanValue >= 40 && RoundManager.self.playerActor.sanValue < 60)
+        {
+            temp = 3;
+     
+        }
+        if (RoundManager.self.playerActor.sanValue >= 20 && RoundManager.self.playerActor.sanValue < 40)
+        {
+            temp = 4;
+           
+        }
+        if (RoundManager.self.playerActor.sanValue >= 0 && RoundManager.self.playerActor.sanValue < 20)
+        {
+            temp = 5;
+        }
+
+        if (temp!=anim&&temp!=0)
+        {
+            change = true;
+        }
+        
+        
+    }
+
+    private void Change()
+    {
+        if (timer>=timed)
+        {
+            timer = 0;
+            ske.AnimationName = "beijing" + anim;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+        
+    }
 }
