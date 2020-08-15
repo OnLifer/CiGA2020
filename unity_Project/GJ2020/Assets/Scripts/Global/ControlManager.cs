f20c7239ba61498809dec4412344ea1fa5e5a8f8
Katyusha444 <52458638+Katyusha444@users.noreply.github.com>
2020-08-15 08:58:31 +0800

    简单场景，加个控制游戏进行的类，没做实现

---
+++ unity_Project/GJ2020/Assets/Scripts/Global/ControlManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlManager :MonoSingleton<ControlManager>
+{
+   
+
+    public void OnCardClick()
+    {
+        //播放状态动画？
+
+        //敌人受击动画？
+
+        //发生实际效果
+
+        //敌人行动
+
+        //回合结束
+    }
+}
