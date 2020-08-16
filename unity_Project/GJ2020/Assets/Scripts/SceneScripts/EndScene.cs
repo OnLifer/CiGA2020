dfaa31e23d4726b49afb9c1c33efb1e009016888
Katyusha444 <52458638+Katyusha444@users.noreply.github.com>
2020-08-16 09:17:49 +0800

    场景贴上了图  做了结尾画面

---
+++ unity_Project/GJ2020/Assets/Scripts/SceneScripts/EndScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float timer;
+    float timed=3;
+
+    private void Update()
+    {
+        if (timer>=timed)
+        {
+            timer = 0;
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+}
